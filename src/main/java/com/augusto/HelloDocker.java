7c1bee1ba71c8e2754a02fe3a08f4706364d1767
augustosilveriosoares <falecomsilverio@gmail.com>
2024-09-12 22:07:28 -0300

    Iniciando com docker

---
+++ src/main/java/com/augusto/HelloDocker.java
@@ -0,0 +1,21 @@
+package com.augusto;
+
+
+public class HelloDocker {
+	
+	private final String content;
+	private final String environment;
+	
+	public HelloDocker(String content, String environment) {
+		this.content = content;
+		this.environment = environment;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+}
